e2c01e2721345bcc45c16539863fceaf4f2530c6
leoyulee <leeleoy@dunwoody.edu>
2020-02-04 16:15:45 -0600

    Small changes.

--- project1/Player.cs
+++ project1/Player.cs
@@ -12,6 +12,10 @@ namespace project1
         private string position;
         private int salary;
         
+        public Player()
+        {
+
+        }
         public Player(string lastName, string firstName, string homeState, string position, int salary)
         {
             this.lastName = lastName;
@@ -19,6 +23,26 @@ namespace project1
             this.homeState = homeState;
             this.position = position;
             this.salary = salary;
+        }
+        public void setFirstName(in string firstName)
+        {
+            this.firstName = firstName;
+        }
+        public void setLastName(in string lastName)
+        {
+            this.lastName = lastName;
+        }
+        public void setHome(in string Home)
+        {
+            this.homeState = Home;
+        }
+        public void setPosition(in string position)
+        {
+            this.position = position;
+        }
+        public void setSalary(in int salary)
+        {
+            this.salary = salaray;
         }
         public string getName(in bool lastFirst)
         {
@@ -39,7 +63,7 @@ namespace project1
         public int getSalary()
         {
             string stringedSalary = salary.ToString();
-            string formattedSalary = 
+            //string formattedSalary = 
             return this.salary;
         }
         override public string ToString()
--- project1/Program.cs
+++ project1/Program.cs
@@ -4,8 +4,15 @@ namespace project1
 {
     class Program
     {
+        private static string response;
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number of players you would like to enter: ");
+            response = Console.ReadLine();
+
+            Player newPlayer = new Player();
+            Console.WriteLine("Welcome! Insert the player's credentials here.");
+            Console.ReadLine();
             Player Burrow = new Player("Burrow", "Joe", "LSU", "Quarterback", 26400100);
             Console.WriteLine("Hello World!");
             Console.WriteLine(Burrow.ToString());
