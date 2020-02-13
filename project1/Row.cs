1d6b0286848ad75fb3b01ddb8e39ed23d04de77b
leoyulee <leeleoy@dunwoody.edu>
2020-02-13 13:28:47 -0600

    new file:   Row.cs new file:   Table.cs

---
+++ project1/Row.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project1
+{
+    class Row
+    {
+        public readonly string Label;
+        private List<Player> Players;
+        public Row(String Label, Player[] Players)
+        {
+            this.Label = Label;
+            this.Players = new List<Player>();
+            foreach (Player player in Players)
+            {
+                this.AddPlayer(player);
+            }
+            this.Players.TrimExcess();
+        }
+        public void AddPlayer(Player player)
+        {
+            this.Players.Add(player);
+        }
+        public override string ToString()
+        {
+
+            return base.ToString();
+        }
+        private List<Player> GetPlayerList()
+        {
+            return Players;
+        }
+    }
+}
---
+++ project1/Table.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project1
+{
+    class Table
+    {
+        private readonly int TabLength = 8;
+        private readonly String[] Header = { "Position", "The Best", "2nd Best", "3rd Best", "4th Best", "5th Best" };
+        private int minColumnLength;
+        private List<Row> Rows;
+        public Table()
+        {
+            this.Rows = new List<Row>();
+        }
+        public Table(Row[] FilledRows)
+        {
+            this.Rows = new List<Row>();
+            foreach (Row row in FilledRows)
+            {
+                this.AddRow(row);
+            }
+        }
+        public void PrintTable()
+        {
+            for (int i = -1; i < Rows.Count; i++)
+            {
+                PrintSeparator(TabLength * Header.Length);
+                if (i == -1)
+                {
+                    PrintHeader();
+                }
+                else
+                {
+                    Rows[i].ToString();
+                }
+            }
+        }
+        public void AddRow(Row FilledRow)
+        {
+            this.Rows.Add(FilledRow);
+            this.Rows.TrimExcess();
+        }
+        private void checkRowStrings(List<Player> input)
+        {
+            foreach(Player player in input)
+            {
+                this.CalculateMinColumnLength(player.Institution);
+                this.CalculateMinColumnLength(player.Name);
+            }
+        }
+        private void CalculateMinColumnLength(string input)
+        {
+            int stringLength = input.Length;
+            int minLengthForInput = stringLength / 8;
+            if(minColumnLength < minLengthForInput)
+            {
+                minColumnLength = minLengthForInput;
+            }
+        }
+        private void PrintSeparator(int length)
+        {
+            string output = "";
+            for (int i = 0; i < length; i++)
+            {
+                output += "-";
+            }
+            Console.WriteLine("\n" + output);
+        }
+        private void PrintHeader()
+        {
+            string outputString = "";
+            foreach (string String in Header)
+            {
+                outputString += String + "\t\t";
+            }
+            Console.WriteLine(outputString);
+        }
+    }
+}
